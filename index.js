fb2d611e7401df8e448f61dfa652bd9cf532ece8
David Anderson <fpzero@gmail.com>
2018-02-28 04:56:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(hoilday, days) {
+function holidayCountdown(holiday, days) {
   return `It\'s ${days} days until ${holiday}!`
 }
